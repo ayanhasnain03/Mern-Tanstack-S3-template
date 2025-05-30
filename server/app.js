e6379d9d02972d1d7992764c2681249213986fd8
Ayan <ayanhasnain2572006@gmail.com>
2025-05-31 01:34:11 +0530

    done

--- server/app.js
+++ server/app.js
@@ -5,7 +5,10 @@ import mongoose from "mongoose";
 import cors from "cors";
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin:process.env.CORS_ORIGIN || "http://localhost:3000",
+  methods: "GET,POST,PUT,DELETE",
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/products", productRoutes);
